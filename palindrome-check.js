83d0b856cf8e3e8281b12892c203f9c6322c9525
Eric Njogu <kunadawa@gmail.com>
2019-10-17 18:26:22 +0300

    feat: implementing a palindrome checking function

--- palindrome-check.js
+++ palindrome-check.js
@@ -2,7 +2,13 @@
 * returns true when a string is a palindrome, false otherwise
 */
 function palindrome_check(target) {
-  return false;
+  squeezed_str = target.toLowerCase().replace(/[\s,'’]+/g, '');
+  console.log(`squeezed string is '${squeezed_str}'`)
+  array1 = squeezed_str.split('');
+  array2 = Array.from(array1);
+  array2.reverse();
+
+  return array1.join('') == array2.join('');
 }
 
 module.exports = palindrome_check;
