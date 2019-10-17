4a714caba3df5c722c4a27e43a34966a4d79bdf8
Eric Njogu <kunadawa@gmail.com>
2019-10-17 16:06:44 +0300

    feat: buggy fibonacci sequence attempt

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,18 @@
 return a fibonacci series till the number shown
 */
 function fibonacci(limit) {
-  return [];
+  // loop up to limit
+  var result = [];
+  if (limit == 1) {
+    result = [0];
+  } else if (limit == 2) {
+    result =  [0, 1];
+  }
+  var i;
+  for (i = 2; i <= limit; i++) {
+    result[i] = result[i-1] + result[i-2];
+  }
+  return result;
 };
 
 module.exports = fibonacci;
