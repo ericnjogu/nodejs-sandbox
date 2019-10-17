44d41f146cd252f0204a859227e9911d4ce7fb67
Eric Njogu <kunadawa@gmail.com>
2019-10-17 19:24:43 +0300

    feat: finding repeated numbers - unimplemented

---
+++ find_repeated.js
@@ -0,0 +1,14 @@
+/*
+"Write a function that given an array of integers,
+it return a dictionary object whose keys are integers that appear multiple times
+and its values are number of times each number appears.
+
+Example 1
+[1,2,3,4,6,6,7,8,9,5,2,6,1,8] #=> {1:2, 2:2, 6:3, 8:2}"
+*/
+
+function find_repeated() {
+
+}
+
+module.exports = find_repeated;
