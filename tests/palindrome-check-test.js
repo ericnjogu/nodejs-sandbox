be4469b544cbc0541b8aa4a4e406da8c105b1bab
Eric Njogu <kunadawa@gmail.com>
2019-10-18 10:13:31 +0300

    test: refactoring to remove duplicated code

--- tests/palindrome-check-test.js
+++ tests/palindrome-check-test.js
@@ -3,18 +3,10 @@ should = require('should');
 const palindrome_check = require('../palindrome-check');
 
 describe ("testing palindrome check", () => {
-  phrase1 = "this is not a palindrome";
-  it (`should return false for "${phrase1}"`, () => {
-    palindrome_check(phrase1).should.be.false();
-  });
-
-  phrase2 = "Mom";
-  it (`should return true for "${phrase2}"`, () => {
-    palindrome_check(phrase2).should.be.true();
-  });
-
-  phrase3 = "Madam, in Eden, I’m Adam";
-  it (`should return true for "${phrase3}"`, () => {
-    palindrome_check(phrase3).should.be.true();
-  });
+  phrases = {"this is not a palindrome":false, "Mom":true, "Madam, in Eden, I’m Adam":true};
+  for (const [phrase, isPalindrome] of Object.entries(phrases)) {
+    it (`should return ${isPalindrome} for "${phrase}"`, () => {
+      palindrome_check(phrase).should.equal(isPalindrome);
+    });
+  }
 })
